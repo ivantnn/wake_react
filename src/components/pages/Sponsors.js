2f9aff6b29bcc06ee2061abbef84ef922b098dc1
Ivantnn <ivantnn2@gmail.com>
2022-10-13 17:01:15 +0200

    New Changes

--- src/components/pages/Sponsors.js
+++ src/components/pages/Sponsors.js
@@ -7,6 +7,10 @@ export default function Sponsors() {
   <a href='https://www.kelio.com/fr/kelio.html'>
   <img src={require("../../Images/Sponsor.png")} width="100%" height="20%" alt=""/>
   </a>
+  <h2 className='sponsors2'>
+  "Kelio provides you with reliable and precise monitoring of the attendance times, absences and activities of your employees. Thanks to the HR portal that can be customized to match your company's colors and operations, you can easily access all the information you need on a daily basis on Kelio."<br/><br/>
+
+  "Kelio is also complete access control and premises security software. Coupled with your HR software, you condition access to working hours and absence schedules. Your data is shared and you benefit from a single and unique platform!"</h2>
   <a href='https://www.imt-atlantique.fr/fr'>
   <img src={require("../../Images/Sponsor2.png")} width="100%" height="20%" alt=""/>
   </a>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './fonts/fyodor/truetype/Fyodor-Bold.ttf';
+import './fonts/GFSNeohellenic/GFSNeohellenic-Regular.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
