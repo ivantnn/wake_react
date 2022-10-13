2f05fa10f9a0622f657dde5861b7bbfb4e060164
Ivantnn <ivantnn2@gmail.com>
2022-10-13 18:30:38 +0200

    Text fix on FAQ, colorbanner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,10 +13,7 @@ function Home() {
     <h1 className='home' style={{ flex:1, flexWrap: 'wrap' }}>WAKE 2022</h1>
     <h2 className='home2'>
     "AN INTERNATIONAL EVENT AT IMT ATLANTIQUE"<br/><br/>
-    As part of the ArchOps industrial chair sponsored by Kelio, the International Students' Office (BDI) in collaboration with our school IMT Atlantique will organize a cultural and sporting event on December 2, 3 and 4. {'\n'}</h2>
-
-
-    <h2 className='home2'>
+    As part of the ArchOps industrial chair sponsored by Kelio, the International Students' Office (BDI) in collaboration with our school IMT Atlantique will organize a cultural and sporting event on December 2, 3 and 4. {'\n'}<br/><br/>
     This event, World Atlantic Kelio Event, will gather around 200 international students from different engineering schools in Nantes, for a week-end. This is the first of two editions on the horizon of Paris 2024 Olympic Games ! The values ​​of sharing and openness are the driving force behind this event, through sporting and cultural encounters.<br/><br/><br/>
 
 
