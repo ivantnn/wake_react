2bc45705b68aa4700c369c4510d856dd9b0a8d02
Ivantnn <ivantnn2@gmail.com>
2022-10-24 18:26:13 +0200

    Sponsors page done

--- src/components/pages/Sponsors.js
+++ src/components/pages/Sponsors.js
@@ -11,7 +11,7 @@ export default function Sponsors() {
   "Kelio provides you with reliable and precise monitoring of the attendance times, absences and activities of your employees. Thanks to the HR portal that can be customized to match your company's colors and operations, you can easily access all the information you need on a daily basis on Kelio."<br/><br/>
   "Kelio is also complete access control and premises security software. Coupled with your HR software, you condition access to working hours and absence schedules. Your data is shared and you benefit from a single and unique platform!"</h2>
   <a href='https://www.imt-atlantique.fr/fr'>
-  <img src={require("../../Images/Sponsor2.png")} width="30%" height="10%" alt=""/>
+  <img src={require("../../Images/Sponsor2.png")} width="30%" height="10%" alt="" class='center_image'/>
   </a>
   </>);
 }
